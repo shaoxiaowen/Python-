d2248f1550cf300c0a2c2fe468180f60afff6fa0
shaoxiaowen <44525688+shaoxiaowen@users.noreply.github.com>
2019-05-09 23:37:02 +0800

    Python3实现火车票查询

---
+++ Python3实现火车票查询/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='tickets',
+    py_modules=['tickets','stations'],
+    install_requires=['requests','docopt','prettytable','colorama'],
+    entry_points={
+        'console_scripts':['tickets=tickets:cli']
+    }
+)
+# 执行 python3 setup.py install 安装
+# tickets 北京 上海 2019-05-10 查询
