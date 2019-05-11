9b4a7fe1124d17d5b82067b9ea7006496729d4f5
shaoxiaowen <44525688+shaoxiaowen@users.noreply.github.com>
2019-05-11 17:15:07 +0800

    代码重构

--- Python3实现简单的Web服务器/server.py
+++ Python3实现简单的Web服务器/server.py
@@ -12,19 +12,44 @@ class ServerException(Exception):
 # test方法用来判断是否符合该类指定的条件，act则是符合条件时的处理函数。
 # 其中的handler是对RequestHandler实例的引用，通过它，我们就能调用handle_file进行响应。
 
-class case_no_file(object):
+
+class base_case(object):
+    '''条件处理基类'''
+
+    def handle_file(self, handler):
+        try:
+            print(handler.full_path)
+            with open(handler.full_path, 'rb') as reader:
+                content = reader.read()
+            handler.send_content(content)
+        except IOError as msg:
+            msg = "'{0}' cannot be read: {1}".format(handler.full_path, msg)
+            handler.handle_error(msg)
+    # 获取主页路径
+
+    def index_path(self, handler):
+        return os.path.join(handler.full_path, 'index.html')
+
+    def test(self, handler):
+        assert False, 'Not implemented.'
+
+    def act(self, handler):
+        assert False, "Not implemented."
+
+
+class case_no_file(base_case):
     '''该路径不存在'''
 
     # 路径不存在 返回true，不存在返回false
     def test(self, handler):
         return not os.path.exists(handler.full_path)
-    # 路径不存在 则抛出异常
 
+    # 路径不存在 则抛出异常
     def act(self, handler):
         raise ServerException("'{0}' not found".format(handler.path))
 
 
-class case_existing_file(object):
+class case_existing_file(base_case):
     '''该路径是文件'''
 
     # 如果是文件 则返回true，不是文件 则返回false
@@ -33,10 +58,10 @@ class case_existing_file(object):
     # 处理文件
 
     def act(self, handler):
-        handler.handle_file(handler.full_path)
+        self.handle_file(handler)
 
 
-class case_always_fail(object):
+class case_always_fail(base_case):
     '''所有情况都不符合时的默认处理类'''
 
     def test(self, handler):
@@ -46,26 +71,39 @@ class case_always_fail(object):
         raise ServerException("Unknown object '{0}'".format(handler.path))
 
 # 返回index.html
-class case_directory_index_file(object):
-    # 获取index.html的全路径
-    def index_path(self,handler):
-        return os.path.join(handler.full_path,'index.html')
+
+
+class case_directory_index_file(base_case):
+    '''在根路径下返回主页文件'''
     # 判断目标路径是否是目录&&目录下是否有index.html
-    def test(self,handler):
+
+    def test(self, handler):
         return os.path.isdir(handler.full_path) and \
             os.path.isfile(self.index_path(handler))
     # 响应index.html的内容
-    def act(self,handler):
-        handler.handle_file(self.index_path(handler))
 
-class case_cgi_file(object):
+    def act(self, handler):
+        handler.full_path=self.index_path(handler)
+        self.handle_file(handler)
+
+
+class case_cgi_file(base_case):
     '''脚本文件处理'''
-    def test(self,handler):
+
+    def run_cgi(self, handler):
+        data = subprocess.check_output(
+            ["python3", handler.full_path], shell=False)
+        handler.send_content(data)
+
+    def test(self, handler):
         return os.path.isfile(handler.full_path) and \
             handler.full_path.endswith('.py')
-    def act(self,handler):
-        #运行脚本文件
-        handler.run_cgi(handler.full_path)
+
+    def act(self, handler):
+        # 运行脚本文件
+        self.run_cgi(handler)
+
+
 class RequestHandler(BaseHTTPRequestHandler):
     '''处理请求并返回页面'''
 
@@ -103,32 +141,27 @@ class RequestHandler(BaseHTTPRequestHandler):
         # page = self.create_page()
         # self.send_content(page)
         try:
+
+            # 访问根路径 会请求两个  第一次的请求路径是127.0.0.1：8080/favicon.ico
+            # 第二次才是127.0.0.1：8080/
+            # if self.path == "/favicon.ico":
+            #     return
             # 文件完整路径
             # os.getcwd() 是当前的工作目录
             # self.path 保存了请求的相对路径
             self.full_path = os.getcwd()+self.path
-
+            print(self.full_path)
             # 遍历所有可能的情况
             for case in self.Cases:
                 # 如果满足该类情况
                 if case.test(self):
-                    #调用响应的act函数
+                    # 调用响应的act函数
+                    print(case.__doc__)
                     case.act(self)
                     break
         # 处理异常
         except Exception as msg:
             self.handle_error(msg)
-
-    # def create_page(self):
-    #     values = {
-    #         'date_time'  :  self.date_time_string(),
-    #         'client_host':  self.client_address[0],
-    #         'client_port':  self.client_address[1],
-    #         'command'    :  self.command,
-    #         'path'       :  self.path
-    #     }
-    #     page=self.Page.format(**values)
-    #     return page
 
     def send_content(self, page):
         self.send_response(200)
@@ -137,25 +170,11 @@ class RequestHandler(BaseHTTPRequestHandler):
         self.end_headers
         # self.wfile.write(page.encode('utf-8'))
         self.wfile.write(page)
-
-    def handle_file(self, full_path):
-        try:
-            # 以二进制的方式打开文件
-            with open(full_path, 'rb') as reader:
-                content = reader.read()
-            self.send_content(content)
-        except IOError as msg:
-            msg = "'{0}' cannot be read:{1}".format(self.path, msg)
-            self.handle_error(msg)
-
     # 处理出现错误的情况
+
     def handle_error(self, msg):
         content = self.Error_Page.format(path=self.path, msg=msg)
         self.send_content(content.encode('utf-8'))
-    
-    def run_cgi(self,full_path):
-        data=subprocess.check_output(["python3",full_path],shell=False)
-        self.send_content(data)
 
 
 if __name__ == '__main__':
