49b87b0eac4e26e69b11b3c98e3e9e5ad1166e76
shaoxiaowen <44525688+shaoxiaowen@users.noreply.github.com>
2019-07-30 23:33:33 +0800

    做成命令行使用

--- Python生成个性二维码/myqrcode.py
+++ Python生成个性二维码/myqrcode.py
@@ -1,45 +1,78 @@
+# -*- coding: utf-8 -*-
+'''
+# Created on 七月-30-19 22:29
+# myqrcode.py
+# @author: shaoxiaowen
+# @description: myqr可以将网页，文字 都转为二维码
+'''
 from MyQR import myqr
+import argparse
 
-# myqr可以将网页，文字 都转为二维码
-
+# words="https://blog.csdn.net/fighting_sxw",
 # 普通二维码
-
-# myqr.run(
-#     words="https://blog.csdn.net/fighting_sxw",
-#     save_name='myblog.png'
-#     )
+def generate_normal_qrcode(words):
+    myqr.run(
+        words=words,
+        save_name='myQrCode.png'
+    )
 
 #
 
 # 带图片的艺术二维码：黑白
-# myqr.run(
-#     words="https://blog.csdn.net/fighting_sxw",
-#     picture='1_fighting_sxw.jpg',
-#     save_name="myblog1.png"
-# )
+
+# picture='1_fighting_sxw.jpg',
+def generate_blackAndWhite_qrcode(words,picture):
+    myqr.run(
+        words=words,
+        picture=picture,
+        save_name="myQrCode.png"
+    )
 
 # 带图片的艺术二维码：彩色
-# myqr.run(
-#     words="https://blog.csdn.net/fighting_sxw",
-#     picture='1_fighting_sxw.jpg',
-#     colorized=True,
-#     save_name="myblog2.png"
-# )
 
-#动态二维码
 
-myqr.run(
-    words="https://blog.csdn.net/fighting_sxw",
-    picture='新垣结衣.gif',
-    colorized=True,
-    save_name="myblog3.gif"
-)
+def generate_color_qrcode(words,picture):
+    myqr.run(
+        words=words,
+        picture=picture,
+        colorized=True,
+        save_name="myQrCode.png"
+    )
 
-#将文字生成二维码
+# 动态二维码
 
-myqr.run(
-    words="hello python",
-    colorized=True,
-    save_name="text.png"
-)
+# picture='新垣结衣.gif'
+def generate_gif_qrcode(words,picture):
+    myqr.run(
+        words=words,
+        picture=picture,
+        colorized=True,
+        save_name="myQrCode.gif"
+    )
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.description = "Python生成个性二维码工具"
+    parser.add_argument('-n',"--normal", help="普通二维码",action="store_true")
+    parser.add_argument('-baw',"--blackAndWhite", help="黑白二维码",action="store_true")
+    parser.add_argument('-c',"--color", help="彩色二维码",action="store_true")
+    parser.add_argument('-g',"--gif", help="动态二维码",action="store_true")
+    parser.add_argument('-w','--words',help="需要转换的内容")
+    parser.add_argument('-p','--picture',help="背景图片地址")
+
+    args = parser.parse_args()
+    if args.normal:
+        print("普通二维码")
+        generate_normal_qrcode(args.words)
+    elif args.blackAndWhite:
+        print("黑白二维码")
+        generate_blackAndWhite_qrcode(args.words,args.picture)
+    elif args.color:
+        print("彩色二维码")
+        generate_color_qrcode(args.words,args.picture)
+    elif args.gif:
+        print("动态二维码")
+        generate_gif_qrcode(args.words,args.picture)
 
